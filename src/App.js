a8b8b7e37fcbde2a59523d752db99d764eb1df20
subreena <subreena20@gmail.com>
2020-09-03 22:46:05 +0600

    learnt pagination in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import { useState } from 'react';
+import Post from './Components/Posts/Post';
+import Pagination from './Components/Pagination/Pagination';
 
 function App() {
+  const [posts , setPosts] = useState([]);
+  const [loading , setLoading] = useState(false);
+  const [currentPage , setCurrentPage] = useState(1);
+  const [ postsPerPage] = useState(10);
+  
+  useEffect( () =>{
+ 
+    const fetchPosts = async () =>{
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(res.data);
+      setLoading(false);
+    }
+fetchPosts();
+
+  },[]);
+
+  // GET CURRENT POSTS
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost , indexOfLastPost)
+
+
+  // PAGINATE
+const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog Posts</h1>
+        <Post posts={currentPosts} loading={loading}></Post>
+        <Pagination paginate={paginate} postsPerPage={postsPerPage} totalPosts={posts.length}></Pagination>
+      
+       
     </div>
   );
 }
---
+++ src/Components/Posts/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Post = ({posts , loading}) => {
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+    return (
+      <ul className="list-group mb-4">
+          {
+            posts.map(post => <li key={post.id} className="list-group-item">{post.title}</li>)
+          }
+      </ul>
+    );
+};
+
+export default Post;
